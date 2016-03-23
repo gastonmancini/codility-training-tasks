8c9c4fc51209ed5bd72588632e367df6d7d00b42
Gaston Mancini <gastonmancini@gmail.com>
2016-03-23 01:11:53 -0300

    Lesson 4 - Counting Elements - FrogRiverOne test

---
+++ src/CodilityTrainingTasks/Tests/Lesson 4/FrogRiverOneTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingTasks.Lesson_4;
+
+namespace Tests.Lesson_4
+{
+    [TestClass]
+    public class FrogRiverOneTest
+    {
+        private FrogRiverOne _frogRiverOne;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _frogRiverOne = new FrogRiverOne();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void FrogRiverOne_Example_Test()
+        {
+            var array = new int[8];
+            array[0] = 1;
+            array[1] = 3;
+            array[2] = 1;
+            array[3] = 4;
+            array[4] = 2;
+            array[5] = 3;
+            array[6] = 5;
+            array[7] = 4;
+            Test(5, array, 6);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+
+        [TestMethod]
+        public void FrogRiverOne_Array_With_Single_Element()
+        {
+            var array = new[] {5};
+            Test(5, array, -1);
+        }
+
+        [TestMethod]
+        public void FrogRiverOne_Never_Cross_The_River()
+        {
+            var array = new int[8];
+            array[0] = 1;
+            array[1] = 3;
+            array[2] = 1;
+            array[3] = 4;
+            array[4] = 2;
+            array[5] = 3;
+            array[6] = 6;
+            array[7] = 4;
+            Test(6, array, -1);
+        }
+
+        #endregion
+
+        private void Test(int x, int[] a, int expectedResult)
+        {
+            var result = _frogRiverOne.solution(x, a);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 4/FrogRiverOne.cs
@@ -0,0 +1,25 @@
+﻿namespace TrainingTasks.Lesson_4
+{
+    public class FrogRiverOne
+    {
+        public int solution(int X, int[] A)
+        {
+            var pathArray = new int[X + 1];
+
+            var jumps = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] > X || pathArray[A[i]] != 0) continue;
+
+                pathArray[A[i]] = A[i];
+                jumps++;
+
+                if (jumps == X) return i;
+            }
+
+            return -1;
+        }
+        
+    }
+}
