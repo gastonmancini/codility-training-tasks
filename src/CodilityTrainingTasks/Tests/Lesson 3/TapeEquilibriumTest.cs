36b8b41e0f4f5990c8193a0a108cf8165c5d0e74
Gaston Mancini <gastonmancini@gmail.com>
2016-03-22 22:40:39 -0300

    Lesson 3 - Time Complexity - TapeEquilibrium test

---
+++ src/CodilityTrainingTasks/Tests/Lesson 3/TapeEquilibriumTest.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingTasks.Lesson_3;
+
+namespace Tests.Lesson_3
+{
+    [TestClass]
+    public class TapeEquilibriumTest
+    {
+        private TapeEquilibrium _tapeEquilibrium;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _tapeEquilibrium = new TapeEquilibrium();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void TapeEquilibrium_Example_Test()
+        {
+            Test(new[] { 3, 1, 2, 4, 3 }, 1);
+        }
+
+        #endregion
+
+        #region Extremes tests
+        
+        [TestMethod]
+        public void TapeEquilibrium_Huge_Array_With_Highest_Values()
+        {
+            int size = 100000;
+
+            var hugeArray = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                hugeArray[i] = 1000;
+            }
+
+            Test(hugeArray, 0);
+        }
+
+        [TestMethod]
+        public void TapeEquilibrium_Small_Array_With_Smallest_Values()
+        {
+            var smallArray = new[] { -1000, -1000 };
+            
+            Test(smallArray, 0);
+        }
+
+        #endregion
+
+        private void Test(int[] array, int expectedResult)
+        {
+            var result = _tapeEquilibrium.solution(array);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 3/TapeEquilibrium.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TrainingTasks.Lesson_3
+{
+    public class TapeEquilibrium
+    {
+        public int solution(int[] A)
+        {
+            int arrayLenght = A.Length;
+
+            if (arrayLenght < 2 || arrayLenght > 100000)
+            {
+                throw new Exception("N is an integer within the range [2..100,000]");
+            }
+
+            int rightSum = 0;
+
+            for (int i = 0; i < arrayLenght; i++)
+            {
+                rightSum += A[i];
+            }
+
+            int leftSum = 0;
+            int result = -1;
+
+            for (int i = arrayLenght - 1; i > 0; i--)
+            {
+                leftSum += A[i];
+                rightSum -= A[i];
+
+                var difference = leftSum - rightSum;
+
+                if (difference < 0)
+                {
+                    difference *= -1;
+                }
+
+                if (result > difference || result == -1)
+                {
+                    result = difference;
+                }
+            }
+
+            return result;
+        }
+    }
+}
