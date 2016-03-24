f22687f2fd52ffe3e3138ea0a6c1d59c2d15bbee
Gaston Mancini <gastonmancini@gmail.com>
2016-03-24 18:50:01 -0300

    Lesson 5 - Prefix Sums - CountDiv test

---
+++ src/CodilityTrainingTasks/Tests/Lesson 5/CountDivTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingTasks.Lesson_5;
+
+namespace Tests.Lesson_5
+{
+    [TestClass]
+    public class CountDivTest
+    {
+        private CountDiv _countDiv;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _countDiv = new CountDiv();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void CountDiv_Example_Test()
+        {
+            Test(6, 11, 2, 3);
+        }
+
+        #endregion
+
+        #region Extremes tests
+        
+        // TODO
+
+        #endregion
+
+        private void Test(int A, int B, int K, int expectedResult)
+        {
+            var result = _countDiv.solution(A, B, K);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 5/CountDiv.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TrainingTasks.Lesson_5
+{
+    public class CountDiv
+    {
+        public int solution(int A, int B, int K)
+        {
+            return (int)(Math.Floor((double)B / K) - Math.Floor((double)(A - 1) / K));
+        }
+
+        //private int ON(int A, int B, int K)
+        //{
+        //    // O(N)
+
+        //    int count = 0;
+
+        //    for (int i = A; i <= B; i++)
+        //    {
+        //        if (i % K == 0) count++;
+        //    }
+
+        //    return count;
+        //}
+    }
+}
