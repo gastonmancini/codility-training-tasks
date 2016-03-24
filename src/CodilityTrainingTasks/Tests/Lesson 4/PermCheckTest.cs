ef5eec3cbb3f733f85b3f4599473ae1b1fc171eb
Gaston Mancini <gastonmancini@gmail.com>
2016-03-24 15:15:26 -0300

    Lesson 4 - Counting Elements - PermCheck test

---
+++ src/CodilityTrainingTasks/Tests/Lesson 4/PermCheckTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingTasks.Lesson_4;
+
+namespace Tests.Lesson_4
+{
+    [TestClass]
+    public class PermCheckTest
+    {
+        private PermCheck _permCheck;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _permCheck = new PermCheck();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void FrogRiverOne_Example_Test_Is_A_Permutation()
+        {
+            var array = new int[4];
+            array[0] = 4;
+            array[1] = 1;
+            array[2] = 3;
+            array[3] = 2;
+            Test(array, 1);
+        }
+
+        [TestMethod]
+        public void FrogRiverOne_Example_Test_Is_Not_A_Permutation()
+        {
+            var array = new int[3];
+            array[0] = 4;
+            array[1] = 1;
+            array[2] = 3;
+            Test(array, 0);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+
+        private void Test(int[] a, int expectedResult)
+        {
+            var result = _permCheck.solution(a);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 4/PermCheck.cs
@@ -0,0 +1,44 @@
+﻿namespace TrainingTasks.Lesson_4
+{
+    public class PermCheck
+    {
+        public int solution(int[] A)
+        {
+            var arrayAux = new int[A.Length + 1];
+            
+            for (int i = 0; i<A.Length; i++)
+            {
+                // Out of range
+                if (A[i] < 1 || A[i] > A.Length) return 0;
+
+                // Duplicated elem
+                if (arrayAux[A[i]] != 0) return 0;
+                
+                arrayAux[A[i]] = A[i];
+            }
+
+            return 1;
+        }
+        
+        //private int NlogN(int[] A)
+        //{
+        //    // O(N * log(N))
+            
+        //    var intArr = Enumerable.Range(1, A.Length).Select(x => x).ToArray();
+
+        //    return A.OrderBy(x => x).SequenceEqual(intArr) ? 1 : 0;
+        //}
+
+        //private int N2(int[] A)
+        //{
+        //    // O(N ** 2)
+
+        //    for (int i = 1; i <= A.Length; i++)
+        //    {
+        //        if (!A.Contains(i)) return 0;
+        //    }
+
+        //    return 1;
+        //}
+    }
+}
