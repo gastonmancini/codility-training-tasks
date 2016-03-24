6a28f34eaf192f0cc97a78a986467fa26169c646
Gaston Mancini <gastonmancini@gmail.com>
2016-03-24 17:02:10 -0300

    Lesson 4 - Counting Elements - MissingInteger test

---
+++ src/CodilityTrainingTasks/Tests/Lesson 4/MissingIntegerTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingTasks.Lesson_4;
+
+namespace Tests.Lesson_4
+{
+    [TestClass]
+    public class MissingIntegerTest
+    {
+        private MissingInteger _missingInteger;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _missingInteger = new MissingInteger();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void MissingInteger_Example_Test()
+        {
+            var array = new int[6];
+            array[0] = 1;
+            array[1] = 3;
+            array[2] = 6;
+            array[3] = 4;
+            array[4] = 1;
+            array[5] = 2;
+            Test(array, 5);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+
+        private void Test(int[] a, int expectedResult)
+        {
+            var result = _missingInteger.solution(a);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 4/MissingInteger.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace TrainingTasks.Lesson_4
+{
+    public class MissingInteger
+    {
+        public int solution(int[] A)
+        {
+            var list = A.Distinct().Where(x => x > 0).ToList();
+            list.Sort();
+
+            int count = 1;
+
+            foreach (var number in list)
+            {
+                if (number != count) return count;
+                count++;
+            }
+
+            return list.Count + 1;
+        }
+    }
+}
