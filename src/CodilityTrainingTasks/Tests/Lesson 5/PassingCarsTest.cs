902d540ad82555960f20a1b70cd1653878357c77
Gaston Mancini <gastonmancini@gmail.com>
2016-03-24 18:21:33 -0300

    Lesson 5 - Prefix Sums - PassingCars test

---
+++ src/CodilityTrainingTasks/Tests/Lesson 5/PassingCarsTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingTasks.Lesson_5;
+
+namespace Tests.Lesson_5
+{
+    [TestClass]
+    public class PassingCarsTest
+    {
+        private PassingCars _passingCars;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _passingCars = new PassingCars();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void PassingCars_Example_Test()
+        {
+            var array = new int[5];
+            array[0] = 0;
+            array[1] = 1;
+            array[2] = 0;
+            array[3] = 1;
+            array[4] = 1;
+            Test(array, 5);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+
+        // TODO
+
+        #endregion
+
+        private void Test(int[] a, int expectedResult)
+        {
+            var result = _passingCars.solution(a);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 5/PassingCars.cs
@@ -0,0 +1,23 @@
+﻿namespace TrainingTasks.Lesson_5
+{
+    public class PassingCars
+    {
+        public int solution(int[] A)
+        {
+            int zeros = 0;
+            long total = 0;
+            
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == 0) 
+                    zeros++;
+                else
+                    total = total + zeros;
+            }
+
+            if (total > 1000000000) return -1;
+            
+            return (int)total;
+        }
+    }
+}
