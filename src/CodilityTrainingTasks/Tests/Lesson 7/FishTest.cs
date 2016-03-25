7ac1fc8a456e5eb5e58a3badf7d9825710fcd293
Gaston Mancini <gastonmancini@gmail.com>
2016-03-25 18:02:09 -0300

    Lesson 7 - Stacks and Queues - Fish test

---
+++ src/CodilityTrainingTasks/Tests/Lesson 7/FishTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingTasks.Lesson_7;
+
+namespace Tests.Lesson_7
+{
+    [TestClass]
+    public class DistinctTest
+    {
+        private Fish _fish;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _fish = new Fish();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void Distinct_Example_Test()
+        {
+            var A = new int[5];
+            A[0] = 4;
+            A[1] = 3;
+            A[2] = 2;
+            A[3] = 1;
+            A[4] = 5;
+
+            var B = new int[5];
+            B[0] = 0;
+            B[1] = 1;
+            B[2] = 0;
+            B[3] = 0;
+            B[4] = 0;
+
+            Test(A, B, 2);
+        }
+
+        #endregion
+
+        #region Extremes tests
+        
+        // TODO
+
+        #endregion
+
+        private void Test(int[] A, int[] B, int expectedResult)
+        {
+            var result = _fish.solution(A, B);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 7/Fish.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TrainingTasks.Lesson_7
+{
+    public class Fish
+    {
+        public int solution(int[] A, int[] B)
+        {
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < B.Length; i++)
+            {
+                // Push the first fish
+                if (stack.Count == 0)
+                {
+                    stack.Push(i);
+                    continue;
+                }
+
+                // Get the previous fish
+                var previous = stack.Peek();
+                
+                // The current fish goes upstream and the previous fish goes downstream
+                if (B[previous] == 1 && B[i] == 0 )
+                {
+                    bool fishAlive = true;
+
+                    // There is any fish and they go in different directions
+                    while (stack.Any() && B[previous] != B[i])
+                    {
+                        if (A[previous] < A[i])
+                        {
+                            // The previous fish was eaten
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            // The current fish died
+                            fishAlive = false;
+                            break;
+                        }
+
+                        if (stack.Any()) previous = stack.Peek();
+                    }
+
+                    // If the current fish didn't die
+                    if (fishAlive) stack.Push(i);
+                }
+                else
+                {
+                    // Go in the same direction
+                    stack.Push(i);
+                }
+            }
+
+            return stack.Count;
+        }
+    }
+}
