cc20d54ad0436c26e08bb6c4f6ca844b3511f08e
Gaston Mancini <gastonmancini@gmail.com>
2016-03-25 16:03:07 -0300

    Lesson 6 - Sorting - MaxProductOfThree test

---
+++ src/CodilityTrainingTasks/Tests/Lesson 6/MaxProductOfThreeTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingTasks.Lesson_6;
+
+namespace Tests.Lesson_6
+{
+    [TestClass]
+    public class MaxProductOfThreeTest
+    {
+        private MaxProductOfThree _maxProductOfThree;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _maxProductOfThree = new MaxProductOfThree();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void MaxProductOfThree_Example_Test()
+        {
+            var array = new int[6];
+            array[0] = -3;
+            array[1] = 1;
+            array[2] = 2;
+            array[3] = -2;
+            array[4] = 5;
+            array[5] = 6;
+            Test(array, 60);
+        }
+
+        #endregion
+
+        #region Extremes tests
+        
+        // TODO
+
+        #endregion
+
+        private void Test(int[] A, int expectedResult)
+        {
+            var result = _maxProductOfThree.solution(A);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 6/MaxProductOfThree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TrainingTasks.Lesson_6
+{
+    public class MaxProductOfThree
+    {
+        public int solution(int[] A)
+        {
+            Array.Sort(A);
+            Array.Reverse(A);
+
+            return Math.Max(
+                A[0] * A[1] * A[2],
+                A[0] * A[A.Length - 1] * A[A.Length - 2]);
+        }
+    }
+}
