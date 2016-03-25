adbe1a003f461c3d33c71f1e50f724ae63daba18
Gaston Mancini <gastonmancini@gmail.com>
2016-03-25 20:13:31 -0300

    Lesson 8 - Leader - EquiLeader test - Incomplete

---
+++ src/CodilityTrainingTasks/Tests/Lesson 8/EquiLeaderTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingTasks.Lesson_8;
+
+namespace Tests.Lesson_8
+{
+    [TestClass]
+    public class EquiLeaderTest
+    {
+        private EquiLeader _equiLeader;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _equiLeader = new EquiLeader();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void EquiLeader_Example_Test()
+        {
+            var A = new int[6];
+            A[0] = 4;
+            A[1] = 3;
+            A[2] = 4;
+            A[3] = 4;
+            A[4] = 4;
+            A[5] = 2;
+
+            Test(A, 2);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+
+        private void Test(int[] A, int expectedResult)
+        {
+            var result = _equiLeader.solution(A);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 8/EquiLeader.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TrainingTasks.Lesson_8
+{
+    public class EquiLeader
+    {
+        public int solution(int[] A)
+        {
+            // TODO
+        }
+
+        //public int Sloooooooowww(int[] A)
+        //{
+        //    var list = new List<int>(A);
+        //    int equiLeaderCount = 0;
+
+        //    for (int S = 1; S < A.Length ; S++)
+        //    {
+        //        var slice1 = list.GetRange(0, S);
+        //        var slice2 = list.GetRange(S, A.Length - S);
+
+        //        var slice1Leader = findLeader(slice1);
+        //        var slice2Leader = findLeader(slice2);
+
+        //        if (slice1Leader == slice2Leader)
+        //        {
+        //            equiLeaderCount++;
+        //        }
+
+        //    }
+
+        //    return equiLeaderCount;
+        //}
+
+        //private int findLeader(List<int> list)
+        //{
+        //    int size = 0;
+        //    int value = -1;
+
+        //    foreach (int elem in list)
+        //    {
+        //        if (size == 0)
+        //        {
+        //            size++;
+        //            value = elem;
+        //        }
+        //        else
+        //        {
+        //            if (value != elem)
+        //                size--;
+        //            else
+        //                size++;
+        //        }
+        //    }
+
+        //    int candidate = -1;
+
+        //    if (size > 0) candidate = value;
+
+        //    int count = list.Count(elem => elem == candidate);
+
+        //    if (count > list.Count / 2) return candidate;
+
+        //    return -1;
+        //}
+
+    }
+}
