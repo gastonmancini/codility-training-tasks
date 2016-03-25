11a0ccd9456faced452eae9ea43ace72bdeb80a4
Gaston Mancini <gastonmancini@gmail.com>
2016-03-25 15:11:45 -0300

    Lesson 6 - Sorting - Triangle test

---
+++ src/CodilityTrainingTasks/Tests/Lesson 6/TriangleTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingTasks.Lesson_6;
+
+namespace Tests.Lesson_6
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        private Triangle _triangle;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _triangle = new Triangle();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void Distinct_Example_Test_Exist_Triangular_Triplet()
+        {
+            var array = new int[6];
+            array[0] = 10;
+            array[1] = 2;
+            array[2] = 5;
+            array[3] = 1;
+            array[4] = 8;
+            array[5] = 20;
+            Test(array, 1);
+        }
+
+        [TestMethod]
+        public void Distinct_Example_Test_Not_Exist_Triangular_Triplet()
+        {
+            var array = new int[4];
+            array[0] = 10;
+            array[1] = 50;
+            array[2] = 5;
+            array[3] = 1;
+            Test(array, 0);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+
+        private void Test(int[] A, int expectedResult)
+        {
+            var result = _triangle.solution(A);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 6/Triangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TrainingTasks.Lesson_6
+{
+    public class Triangle
+    {
+        public int solution(int[] A)
+        {
+            Array.Sort(A);
+
+            for (int i = 0; i < A.Length - 2; i++)
+            {
+                if (A[i] >= 0 && A[i] > A[i + 2] - A[i + 1]) return 1;
+            }
+
+            return 0;
+        }
+
+        //private int N3(int[] A)
+        //{
+        //    // O(N ** 3)
+
+        //    var combinations = Enumerable.Range(0, A.Length)
+        //        .SelectMany(
+        //            x =>
+        //            Enumerable.Range(0, A.Length)
+        //            .SelectMany(
+        //            y =>
+        //            Enumerable.Range(0, A.Length)
+        //            .Select(
+        //            z => new Tuple<int, int, int>(x, y, z))));
+
+        //    int P, Q, R;
+
+        //    foreach (var tuple in combinations)
+        //    {
+        //        P = tuple.Item1;
+        //        Q = tuple.Item2;
+        //        R = tuple.Item3;
+
+        //        if ((A[P] + A[Q] > A[R]) &&
+        //            (A[Q] + A[R] > A[P]) &&
+        //            (A[R] + A[P] > A[Q]) &&
+        //            0 <= P && P < Q && Q < R && R < A.Length)
+        //        {
+        //            return 1;
+        //        }
+        //    }
+
+        //    return 0;
+        //}
+
+
+    }
+}
