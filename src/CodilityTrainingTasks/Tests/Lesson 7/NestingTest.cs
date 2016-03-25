49dfaf4185aad836d54057444a5ad4da0ddcf012
Gaston Mancini <gastonmancini@gmail.com>
2016-03-25 18:50:35 -0300

    Lesson 7 - Stacks and Queues - Nesting test

---
+++ src/CodilityTrainingTasks/Tests/Lesson 7/NestingTest.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingTasks.Lesson_7;
+
+namespace Tests.Lesson_7
+{
+    [TestClass]
+    public class NestingTest
+    {
+        private Nesting _nesting;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _nesting = new Nesting();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void Nesting_Example_Test_Properly_Nested()
+        {
+            var s = "(()(())())";
+       
+            Test(s, 1);
+        }
+
+        [TestMethod]
+        public void Nesting_Example_Test_Not_Properly_Nested()
+        {
+            var s = "())";
+
+            Test(s, 0);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        [TestMethod]
+        public void Nesting_Test_Empty_String()
+        {
+            var s = string.Empty;
+
+            Test(s, 1);
+        }
+
+        [TestMethod]
+        public void Nesting_Test_Complex_String_Is_Properly_Nested()
+        {
+            var s = "((((()))))()(())((()))(((((((((())))))))))((((()))))()(())((()))(((((((((())))))))))";
+
+            Test(s, 1);
+        }
+
+        [TestMethod]
+        public void Nesting_Test_Complex_String_Is_Not_Properly_Nested()
+        {
+            var s = "((((()))))()(())((()))(((((((((())))))))))((((()))))((())((()))(((((((((())))))))))";
+
+            Test(s, 0);
+        }
+
+        #endregion
+
+        private void Test(string S, int expectedResult)
+        {
+            var result = _nesting.solution(S);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 7/Nesting.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TrainingTasks.Lesson_7
+{
+    public class Nesting
+    {
+        public int solution(string S)
+        {
+            if (string.IsNullOrEmpty(S)) return 1;
+
+            var count = 0;
+            
+            foreach (char current in S)
+            {
+                if (AreEqual(current, "("))
+                {
+                    count++;
+                }
+                else if (AreEqual(current, ")"))
+                {
+                    count--;
+                }
+
+                if (count < 0) return 0;
+            }
+
+            return count == 0 ? 1 : 0;
+        }
+
+        private static bool AreEqual(char c, string s)
+        {
+            return c == Convert.ToChar(s);
+        }
+    }
+}
