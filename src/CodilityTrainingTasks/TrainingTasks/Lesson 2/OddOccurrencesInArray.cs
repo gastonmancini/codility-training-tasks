c867f9856ec63c5983a78bb39e89b54c1d0d1fce
Gaston Mancini <gastonmancini@gmail.com>
2016-03-26 18:32:28 -0300

    Lesson 2 - Arrays - OddOccurrencesInArray test - XOR solution

--- src/CodilityTrainingTasks/TrainingTasks/Lesson 2/OddOccurrencesInArray.cs
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 2/OddOccurrencesInArray.cs
@@ -6,32 +6,44 @@ namespace TrainingTasks.Lesson_2
     {
         public int solution(int[] A)
         {
+            int unpaired = 0;
 
-            Array.Sort(A);
-
-            int previousValue = A[0];
-            int count = 1;
-
-            for (int i = 1; i < A.Length; i++)
+            for (int i = 0; i < A.Length; i++)
             {
-                if (A[i] == previousValue)
-                {
-                    previousValue = -1;
-                    count = 0;
-                }
-                else
-                {
-                    if (count == 1)
-                    {
-                        break;
-                    }
-
-                    previousValue = A[i];
-                    count++;
-                }
+                unpaired ^= A[i];
             }
 
-            return previousValue;
+            return unpaired;
         }
+
+        //public int solution(int[] A)
+        //{
+        //    // O (N * LOG N)
+        //    Array.Sort(A);
+
+        //    int previousValue = A[0];
+        //    int count = 1;
+
+        //    for (int i = 1; i < A.Length; i++)
+        //    {
+        //        if (A[i] == previousValue)
+        //        {
+        //            previousValue = -1;
+        //            count = 0;
+        //        }
+        //        else
+        //        {
+        //            if (count == 1)
+        //            {
+        //                break;
+        //            }
+
+        //            previousValue = A[i];
+        //            count++;
+        //        }
+        //    }
+
+        //    return previousValue;
+        //}
     }
 }
