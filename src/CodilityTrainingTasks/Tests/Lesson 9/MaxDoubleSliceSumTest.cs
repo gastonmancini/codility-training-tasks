b07c4f9b1d5faec864e6ade3f478d1767eaf368c
Gaston Mancini <gastonmancini@gmail.com>
2016-03-26 16:34:29 -0300

    Lesson 9 - Maximum slice problem - MaxDoubleSliceSum test

---
+++ src/CodilityTrainingTasks/Tests/Lesson 9/MaxDoubleSliceSumTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingTasks.Lesson_9;
+
+namespace Tests.Lesson_9
+{
+    [TestClass]
+    public class MaxDoubleSliceSumTest
+    {
+        private MaxDoubleSliceSum _maxDoubleSliceSum;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _maxDoubleSliceSum = new MaxDoubleSliceSum();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void MaxDoubleSliceSum_Example_Test()
+        {
+            var A = new int[8];
+            A[0] = 3;
+            A[1] = 2;
+            A[2] = 6;
+            A[3] = -1;
+            A[4] = 4;
+            A[5] = 5;
+            A[6] = -1;
+            A[7] = 2;
+
+            Test(A, 17);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+
+        private void Test(int[] A, int expectedResult)
+        {
+            var result = _maxDoubleSliceSum.solution(A);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 9/MaxDoubleSliceSum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TrainingTasks.Lesson_9
+{
+    public class MaxDoubleSliceSum
+    {
+        public int solution(int[] A)
+        {
+            // Calculate the maximum sum subarray with one element missing
+
+            int length = A.Length;
+
+            int[] maxStartingHere = new int[length];
+            int maxSum = 0;
+
+            // Kadane's algorithm in reverse direction.
+            for (int i = length - 2; i > 0; i--)
+            {
+                maxSum = Math.Max(0, A[i] + maxSum);
+                maxStartingHere[i] = maxSum;
+            }
+
+            int[] maxEndingHere = new int[length];
+            maxSum = 0;
+
+            // Kadane's algorithm in forward direction.
+            for (int i = 1; i < length - 1; i++)
+            {
+                maxSum = Math.Max(0, A[i] + maxSum);
+                maxEndingHere[i] = maxSum;
+            }
+
+            int maxDoubleSlice = 0;
+
+            // Iterate arrays simultaneously and choose the 'Y' that has the maximum value of
+            for (int i = 0; i < length - 2; i++)
+            {
+                maxDoubleSlice = Math.Max(maxDoubleSlice, maxEndingHere[i] + maxStartingHere[i + 2]);
+            }
+            return maxDoubleSlice;
+        }
+    }
+}
