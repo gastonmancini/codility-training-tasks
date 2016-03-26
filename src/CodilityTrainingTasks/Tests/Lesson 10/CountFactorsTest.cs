93427d4bdfcd847824779b4e31bc02d420066a18
Gaston Mancini <gastonmancini@gmail.com>
2016-03-26 17:30:31 -0300

    Lesson 10 - Prime and composite numbers - CountFactors test

---
+++ src/CodilityTrainingTasks/Tests/Lesson 10/CountFactorsTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingTasks.Lesson_10;
+
+namespace Tests.Lesson_10
+{
+    [TestClass]
+    public class CountFactorsTest
+    {
+        private CountFactors _countFactors;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _countFactors = new CountFactors();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void CountFactors_Example_Test()
+        {
+            Test(24, 8);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        [TestMethod]
+        public void CountFactors_One()
+        {
+            Test(1, 1);
+        }
+
+        [TestMethod]
+        public void CountFactors_Two()
+        {
+            Test(2, 2);
+        }
+
+        [TestMethod]
+        public void CountFactors_Three()
+        {
+            Test(3, 2);
+        }
+
+        [TestMethod]
+        public void CountFactors_Max_Int()
+        {
+            Test(int.MaxValue, 2);
+        }
+
+        #endregion
+
+        private void Test(int N, int expectedResult)
+        {
+            var result = _countFactors.solution(N);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 10/CountFactors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TrainingTasks.Lesson_10
+{
+    public class CountFactors
+    {
+        public int solution(int N)
+        {
+            // O(sqrt(N))
+
+            if (N == 1) return N;
+
+            int result = 0;
+
+            int i = 1;
+
+            int sqrt = (int) Math.Sqrt(N);
+
+            while (i <= sqrt)
+            {
+                if (N%i == 0) result+=2;
+                i++;
+            }
+
+            if (sqrt * sqrt == N) result--;
+
+            return result;
+        }
+    }
+}
