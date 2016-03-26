4ff6ef5150a30aa753c51742e90d7793eda83a7e
Gaston Mancini <gastonmancini@gmail.com>
2016-03-26 17:46:43 -0300

    Lesson 10 - Prime and composite numbers - MinPerimeterRectangle test

---
+++ src/CodilityTrainingTasks/Tests/Lesson 10/MinPerimeterRectangleTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingTasks.Lesson_10;
+
+namespace Tests.Lesson_10
+{
+    [TestClass]
+    public class MinPerimeterRectangleTest
+    {
+        private MinPerimeterRectangle _minPerimeterRectangle;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _minPerimeterRectangle = new MinPerimeterRectangle();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void MinPerimeterRectangle_Example_Test()
+        {
+            Test(30, 22);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        [TestMethod]
+        public void MinPerimeterRectangle_One()
+        {
+            Test(1, 4);
+        }
+
+        [TestMethod]
+        public void MinPerimeterRectangle_Max()
+        {
+            Test(1000000000, 126500);
+        }
+
+        #endregion
+
+        private void Test(int N, int expectedResult)
+        {
+            var result = _minPerimeterRectangle.solution(N);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 10/MinPerimeterRectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TrainingTasks.Lesson_10
+{
+    public class MinPerimeterRectangle
+    {
+        public int solution(int N)
+        {
+            // O(sqrt(N))
+            int i = 1;
+            int sqrt = (int) Math.Sqrt(N);
+            int minPerimeter = calculatePerimeter(1, N);
+
+            while (i <= sqrt)
+            {
+                if (N%i == 0)
+                {
+                    var currentPerimeter = calculatePerimeter(i, N/i);
+                    if (currentPerimeter < minPerimeter)
+                    {
+                        minPerimeter = currentPerimeter;
+                    }
+                }
+
+                i++;
+            }
+
+            return minPerimeter;
+        }
+
+        private int calculatePerimeter(int A, int B)
+        {
+            return 2*(A + B);
+        }
+    }
+}
