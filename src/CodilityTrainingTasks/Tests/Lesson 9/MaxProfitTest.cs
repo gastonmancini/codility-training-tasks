252d91fd01675fa7aadf28c7c4f851335133ad43
Gaston Mancini <gastonmancini@gmail.com>
2016-03-26 16:01:04 -0300

    Lesson 9 - Maximum slice problem - MaxProfit test

---
+++ src/CodilityTrainingTasks/Tests/Lesson 9/MaxProfitTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingTasks.Lesson_9;
+
+namespace Tests.Lesson_9
+{
+    [TestClass]
+    public class MaxProfitTest
+    {
+        private MaxProfit _maxProfit;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _maxProfit = new MaxProfit();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void MaxProfit_Example_Test()
+        {
+            var A = new int[6];
+            A[0] = 23171;
+            A[1] = 21011;
+            A[2] = 21123;
+            A[3] = 21366;
+            A[4] = 21013;
+            A[5] = 21367;
+
+            Test(A, 356);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        // TODO
+
+        #endregion
+
+        private void Test(int[] A, int expectedResult)
+        {
+            var result = _maxProfit.solution(A);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 9/MaxProfit.cs
@@ -0,0 +1,28 @@
+﻿namespace TrainingTasks.Lesson_9
+{
+    public class MaxProfit
+    {
+        public int solution(int[] A)
+        {
+            if (A == null || A.Length <= 0) return 0;
+            
+            int min = A[0];
+            int maxDiff = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                int currentDiff = A[i] - min;
+                if (currentDiff > maxDiff)
+                {
+                    maxDiff = currentDiff;
+                }
+                if (A[i] < min)
+                {
+                    min = A[i];
+                }
+            }
+
+            return maxDiff;
+        }
+    }
+}
