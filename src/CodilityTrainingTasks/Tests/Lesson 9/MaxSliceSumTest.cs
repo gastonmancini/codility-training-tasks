701a3eeb796ddaf5a101529ccdbf758fc98b5758
Gaston Mancini <gastonmancini@gmail.com>
2016-03-26 16:51:20 -0300

    Lesson 9 - Maximum slice problem - MaxSliceSum test

---
+++ src/CodilityTrainingTasks/Tests/Lesson 9/MaxSliceSumTest.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingTasks.Lesson_9;
+
+namespace Tests.Lesson_9
+{
+    [TestClass]
+    public class MaxSliceSumTest
+    {
+        private MaxSliceSum _maxSliceSum;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _maxSliceSum = new MaxSliceSum();
+        }
+
+        #region Example Tests
+
+        [TestMethod]
+        public void MaxSliceSum_Example_Test()
+        {
+            var A = new int[5];
+            A[0] = 3;
+            A[1] = 2;
+            A[2] = -6;
+            A[3] = -4;
+            A[4] = 0;
+
+            Test(A, 5);
+        }
+
+        #endregion
+
+        #region Extremes tests
+
+        [TestMethod]
+        public void MaxSliceSum_One_Elem_Array()
+        {
+            var A = new int[1];
+            A[0] = 3;
+
+            Test(A, 3);
+        }
+
+        [TestMethod]
+        public void MaxSliceSum_a()
+        {
+            var A = new int[5];
+            A[0] = 3;
+            A[1] = 2;
+            A[2] = -6;
+            A[3] = -4;
+            A[4] = 16;
+
+            Test(A, 6);
+        }
+
+        #endregion
+
+        private void Test(int[] A, int expectedResult)
+        {
+            var result = _maxSliceSum.solution(A);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson 9/MaxSliceSum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TrainingTasks.Lesson_9
+{
+    public class MaxSliceSum
+    {
+        public int solution(int[] A)
+        {
+            int maxSum = 0;
+            int maxSliceSum = int.MinValue;
+
+            // Kadane's algorithm
+            for (int i = 0; i < A.Length; i++)
+            {
+                maxSum = Math.Max(int.MinValue, A[i] + maxSum);
+                if (A[i] > maxSum) maxSum = A[i];
+
+                maxSliceSum = Math.Max(maxSliceSum, maxSum);
+            }
+            
+            return maxSliceSum;
+        }
+    }
+}
