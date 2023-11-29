4a8c88b18a4f20fb4c33a3d2e947f1f4ce84f78d
Gastón Mancini <gmancini@hammer.net>
2023-11-29 16:08:45 -0300

    Lesson 1 - Iterations - BinaryGap

--- src/CodilityTrainingTasks/TrainingTasks/Lesson1-Iterations/BinaryGap.cs
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson1-Iterations/BinaryGap.cs
@@ -34,8 +34,38 @@ namespace TrainingTasks
             }
 
             return maxBinaryGap;
+            // https://codility.com/demo/results/training57BZZ5-W5Z/
         }
 
-        // https://codility.com/demo/results/training57BZZ5-W5Z/
+        // looks like this solutions got 80% score because there are some tests that do not pass in codility,
+        // but they do pass here!
+        // examples of this tests:
+        // N = 561892, got 4 expected 3
+        // N = 74901729, got 6 expected 4
+        // N = 1376796946, got 8 expected 5
+        public int solutionV2(int N)
+        {
+            var binaryRepresentation = Convert.ToString(N, 2);
+            var currentCount = 0;
+            var max = 0;
+
+            for (int i = 0; i < binaryRepresentation.Length; i++)
+            {
+                char number = binaryRepresentation[i];
+                if (number == '1' && currentCount == 0) continue; // nothing to count
+                if (number == '0')
+                {
+                    currentCount++;
+                }
+                else if (number == '1' && currentCount > max)
+                {
+                    max = currentCount;
+                    currentCount = 0;
+                }
+            }
+
+            return max;
+            // https://app.codility.com/demo/results/trainingKRX2NK-PJM/
+        }
     }
 }
