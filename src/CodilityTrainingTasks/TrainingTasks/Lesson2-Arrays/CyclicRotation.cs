4d6cdaaa4ee5bb6a323ac193d65b7b52c4caa926
Gastón Mancini <gmancini@hammer.net>
2023-11-29 16:50:01 -0300

    Lesson 2 - Arrays - CyclicRotation

--- src/CodilityTrainingTasks/TrainingTasks/Lesson2-Arrays/CyclicRotation.cs
+++ src/CodilityTrainingTasks/TrainingTasks/Lesson2-Arrays/CyclicRotation.cs
@@ -23,8 +23,26 @@ namespace TrainingTasks
             }
 
             return aAsList.ToArray();
+            // https://codility.com/demo/results/trainingUQ2T2N-8TQ/
         }
 
-        // https://codility.com/demo/results/trainingUQ2T2N-8TQ/
+        public int[] solutionV2(int[] A, int K)
+        {
+            if (K == 0 || A.Length < 2) return A;
+
+            while (K > 0)
+            {
+                var lastElement = A[A.Length - 1];
+                for (int i = A.Length - 1; i > 0; i--)
+                {
+                    A[i] = A[i - 1];
+                }
+                A[0] = lastElement;
+                K--;
+            }
+
+            return A;
+            // https://app.codility.com/demo/results/trainingJNBMCZ-GHG/
+        }
     }
 }
